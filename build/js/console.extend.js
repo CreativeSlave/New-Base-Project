8f9b6c742303691cbbd0af9855b875fbe882b68f
Andrew Ambrose <andrew@creativeslave.com>
2016-05-04 20:56:50 -0600

    Refactored for simplicity.

--- build/js/console.extend.js
+++ build/js/console.extend.js
@@ -1,130 +1,106 @@
-
 /**
- * Overriding the console to enable testing.
+ * Extending the Console
  */
 (function() {
-    if(console){
-        console.results = [];
-        /**
-         *
-         * @type {{describe: string, express: string, result: boolean, toBe: string}}
-         */
-        console.expression={
-            describe:"",
-            express:null,
-            result:false,
-            toBe:""
-        };
-        /**
-         *
-         * @param description
-         * @param expression
-         */
-        console.expect= function(description, express){
-            console.expression = {
-                describe:description,
-                express:express,
-                result:false,
-                toBe:"",
-                toString: function(){
-                    var pre="The ";
-                    var res=" and it is! \n (result === TRUE)"; //and the result is
-                    if(!console.expression.result){
-                        pre="Sorry, the "; res=" but it is not. \n(result === FALSE)";
-                    }
-                    return pre +console.expression.describe+" '"+ console.expression.express+
-                        "' is expected to be equal to '"+console.expression.toBe+
-                        "' "+res+"\n";
-                }
-            };
-            return console;
-        };
-        /**
-         *
-         * @param arg
-         * @param expressionE
-         */
-        console.assert= function(arg, expressionE){
-            console.expression = {
-                describe:arg,
-                express:console.eval(expressionE),
-                result:false,
-                toBe:""
-            };
-            return console;
-        };
-        /**
-         *
-         * @param expression
-         * @returns {*}
-         */
-        console.eval= function(express){
-            try{
-                return eval(express);
-            } catch(e){
-                console.error(e);
-                return express.toString();
-            } finally{
-                return "";
-            }
-        };
-        /**
-         * console.expect().toBe()
-         * @param testValue
-         * @returns {boolean}
-         */
-        console.toBe= function(testValue){
-            if(console.expression.describe==="" || console.expression.express!=null){
-                console.warn("You have attempted to call console.toBe without setting " +
-                    "console.expect. Your expression should look like this:\n" +
-                    "console.expect('My bike tires colors',{ bike.tires.color }).toBe({ black });");
-            }
-            try{
-                console.expression.toBe = testValue;
-                console.expression.result =  (console.expression.express===console.expression.toBe);
-                console.results.push(console.expression);
-            } catch(e){
-                console.error(e);
-            }
-            var res = (console.expression.result)?" Succedded":" Failed!";
-            console.group("Expection"+res);
-            console.log(console.expression.toString());
-            console.groupEnd("Expection"+res);
-            return (console.expression.result);
-        };
-        /**
-         * console.expect(object).toContain(property);
-         */
-        console.toContain= function(property){
-            if(console.expression.describe==="" || console.expression.express!=null){
-                console.warn("You have attempted to call console.toContain without setting " +
-                    "console.expect. Your expression should look like this:\n" +
-                    "console.expect('My car object ',{ car }).toContain({ wheels });");
-            }
-            try{
-                console.info(console.expression.toString());
-                var ex = console.expression.express;
-                if(ex!=null){
-                    if(typeof ex === 'string'){
-                        return (ex.indexOf(property.toString())==-1);
-                    } else if(typeof ex === 'boolean'){
-                        return (property===ex);
-                    } else if(typeof ex === 'number'){
-                        return (ex.toString().indexOf(property.toString())==-1);
-                    } else if(typeof ex === 'function'){
-                        return (ex.toString().indexOf(property.toString())==-1);
-                    } else if(typeof ex === 'object'){
-                        return ex.hasOwnProperty(property);
-                    } else {
-                        return false;
-                    }
-                }
-            } catch (e){}
-            return false;
-
-        };
-        console.laugh = function(){
-            console.log("Ha ha ha ha... ha... .");
+    "use strict";
+    console.level = ["LOW", "MEDIUM", "HIGH"];
+    console.tests = [];
+    console.printTests = function(){
+        if(console.tests.length>0){
+            console.table(console.tests);
+        } else {
+            console.info("There are not tests to view.");
         }
-    }
+
+    };
+    console.aboutExpect = function(){
+        console.group("What to Expect: About Expect");
+        console.log("This is just a simple tool to test conditions while writing JavaScript code. \n"+
+            "The simple syntax looks like this:\n > "+
+            "console.expect('My bike tires colors',{ bike.tires.color }).toBe('black');\n");
+
+        console.log("\nAnother method is ..contains() \n"+
+            "The simple syntax looks like this:\n > "+
+            "console.expect('My bike tires colors',{ bike }).toBe('tires');\n");
+        console.log("--drew");
+        console.groupEnd("What to Expect: About Expect");
+    };
+    /**
+     * ['console'] is a global Singleton. Preserving logs will preserve your tests.
+     * This encapsulates the test to
+     * @param description
+     * @param expression
+     * @returns {{toBe: toBe}}
+     */
+    console.expect = function(description, expression){
+        var expressionTest = {
+            description: description,
+            expression: expression,
+            testExpression: "",
+            result:false,
+            toBe: function(testExpression){
+//                 if(expressionTest.description==="" || expressionTest.expression!=null){
+//                     console.warn("You have attempted to call console.toBe without setting " +
+//                         "console.expect. \nYour expression should look like this:\n" +
+//                         "console.expect('My bike tires colors',{ bike.tires.color }).toBe('black');");
+//                 }
+                try{
+                    expressionTest.testExpression = testExpression;
+                    expressionTest.result =  (expressionTest.expression===expressionTest.testExpression);
+                    console.tests.push(expressionTest);
+                } catch(e){
+                    console.error(e);
+                }
+                var res = (expressionTest.result)?" Succedded!":" Failed.";
+                console.group("Test"+res);
+                (expressionTest.result) ? console.info(expressionTest.toString()):console.warn(expressionTest.toString());
+                console.groupEnd("Test"+res);
+                return (expressionTest.result);
+            },
+            toContain: function(property){
+                if(expressionTest.description==="" || expressionTest.expression!==null){
+                    console.warn("You have attempted to call console.toContain without setting " +
+                        "console.expect. Your expression should look like this:\n" +
+                        "console.expect('My car object ',{ car }).toContain({ wheels });");
+                }
+                try{
+                    console.info(expressionTest.toString());
+                    var ex = expressionTest.expression;
+                    if(ex!=null){
+                        if(typeof ex === 'string'){
+                            return (ex.indexOf(property.toString())==-1);
+                        } else if(typeof ex === 'boolean'){
+                            return (property===ex);
+                        } else if(typeof ex === 'number'){
+                            return (ex.toString().indexOf(property.toString())==-1);
+                        } else if(typeof ex === 'function'){
+                            return (ex.toString().indexOf(property.toString())==-1);
+                        } else if(typeof ex === 'object'){
+                            return ex.hasOwnProperty(property);
+                        } else {
+                            return false;
+                        }
+                    }
+                } catch (e){}
+                return false;
+            },
+            toString: function(){
+                var pre="The ";
+                var res=" and it is! \n (result === TRUE)"; //and the result is
+                if(!expressionTest.result){
+                    pre="Sorry, the "; res=" but it is not. \n(result === FALSE)";
+                }
+                return pre +expressionTest.description+" '"+ expressionTest.expression+
+                    "' is expected to be equal to '"+expressionTest.testExpression+
+                    "' "+res+"\n";
+            }
+        };
+        return {
+            about: console.log("This is just a simple toll to test while writing JavaScript code. \n"+
+                "The simple syntax looks like this:\n > "+
+                "console.expect('My bike tires colors',{ bike.tires.color }).toBe('black');\n"),
+            toBe: expressionTest.toBe,
+            contain: expressionTest.contain,
+        }
+    };/*void*/
 })();
